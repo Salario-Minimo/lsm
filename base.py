eee6c159f1af5ba8da29f882148fb94f2b8c1bc1
Salario-Minimo <164933157+Salario-Minimo@users.noreply.github.com>
2024-05-02 13:31:18 -0600

    Update base.py

--- base.py
+++ base.py
@@ -4,9 +4,11 @@ import pandas as pd
 señas = pd.DataFrame({"señas":("abril","bien","computadora","arroz", "agosto"),
                       "configuracion":("a","b","c","a","a")})
 
-configuracion = st.selectbox("¿Cuál es la configuración?", ("a","b","c"))
 
 st.header("🧏 Diccionario LSM - Español 📖")
+
+
+configuracion = st.selectbox("¿Cuál es la configuración?", ("a","b","c"))
 
 st.text(configuracion)
 st.text(señas)
