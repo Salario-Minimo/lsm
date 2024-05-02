d212094bbc97d5ad9c7fd45c037ed45abdc123e9
Salario-Minimo <164933157+Salario-Minimo@users.noreply.github.com>
2024-05-02 01:42:37 -0600

    Update base.py

--- base.py
+++ base.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import pandas as pd
 
+señas = pd.DataFrame({"señas":("abril","bien","computadora"),
+                      "configuracion":("a","b","c")})
+
 configuracion = st.selectbox("¿Cuál es la configuración?", ("a","b","c"))
 
 st.text(configuracion)
+st.text(señas)
