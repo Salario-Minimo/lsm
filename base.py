fcde474426029b4b7e4b3fc10e356aeb57cd35bb
Salario-Minimo <164933157+Salario-Minimo@users.noreply.github.com>
2024-05-03 20:35:37 -0600

    Update base.py

--- base.py
+++ base.py
@@ -2,12 +2,13 @@ import streamlit as st
 import pandas as pd
 
 señas = pd.ExcelFile("Señas_lsm.xlsx").parse(0)
+Configuraciones = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
 
 
 st.header("🧏 Diccionario LSM - Español 📖")
 
 
-configuracion = st.selectbox("¿Cuál es la configuración?", ("a","b","c"))
+configuracion = st.selectbox("¿Cuál es la configuración?", Configuraciones)
 
 st.text(señas)
 
