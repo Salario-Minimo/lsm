799babb610fb6fb4070700ae66099dad16e888f3
Salario-Minimo <164933157+Salario-Minimo@users.noreply.github.com>
2024-05-05 15:16:58 -0600

    Update base.py

--- base.py
+++ base.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 señas = pd.ExcelFile("Señas_lsm.xlsx").parse(0)
 Configuraciones = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
-Simetria = ("Singular", "Alternado", "Asimetrico", "Simetrico")
+Simetrias = ("Singular", "Alternado", "Asimetrico", "Simetrico")
 
 
 
@@ -11,6 +11,7 @@ st.header("🧏 Diccionario LSM - Español 📖")
 
 
 configuracion = st.selectbox("¿Cuál es la configuración?", Configuraciones)
+simetria = st.selectbox("¿Cuál es la simetría?", Simetrias)
 
 st.text(señas)
 
