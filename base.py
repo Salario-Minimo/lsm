e96eb338c836bafca6927ab53de369a800c731e9
Salario-Minimo <164933157+Salario-Minimo@users.noreply.github.com>
2024-05-20 13:10:23 -0600

    Update base.py

--- base.py
+++ base.py
@@ -1,20 +1,26 @@
 import streamlit as st
 import pandas as pd
 
+
 señas = pd.ExcelFile("Señas_lsm.xlsx").parse(0)
-Configuraciones = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
+Configuraciones = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
 Simetrias = ("Singular", "Alternado", "Asimetrico", "Simetrico")
-
-
+Zona = ("Mentón", "Mejillas", "Frente", "Pecho")
 
 st.header("🧏 Diccionario LSM - Español 📖")
 
-
 configuracion = st.selectbox("¿Cuál es la configuración?", Configuraciones, index = None)
-simetria = st.selectbox("¿Cuál es la simetría?", Simetrias, index = None)
+simetria = st.selectbox("¿Cuál es la simetría?", Simetrias, index = None
 
-resultado = señas[señas["Configuracion"]==configuracion]
-resultado = resultado[resultado["Simetria"]==simetria]
+if configuracion == None:
+  resultado = señas
+else:
+  resultado = señas[señas["Configuracion"]==configuracion]
+
+if configuracion == None:
+  resultado = resultado
+else:
+  resultado = resultado[resultado["Simetria"]==simetria]
 
 with st.container(height = 500):
   for index, row in resultado.iterrows():
